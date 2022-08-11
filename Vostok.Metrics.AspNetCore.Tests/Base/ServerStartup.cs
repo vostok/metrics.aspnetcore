10d696994854dc10fb97ac11c2763b924fbacc45
Ilya Shumilin <shumilin@skbkontur.ru>
2022-08-11 14:48:19 +0500

    Add helpers

---
+++ Vostok.Metrics.AspNetCore.Tests/Base/ServerStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Vostok.Metrics.AspNetCore.Tests.Base;
+
+internal class ServerStartup
+{
+    private readonly Action<IApplicationBuilder> setup;
+    private readonly RequestDelegate requestDelegate;
+
+    public ServerStartup(Action<IApplicationBuilder> setup, RequestDelegate requestDelegate)
+    {
+        this.setup = setup;
+        this.requestDelegate = requestDelegate
+                               ?? (context =>
+                               {
+                                   context.Response.StatusCode = 200;
+                                   return Task.CompletedTask;
+                               });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+        setup?.Invoke(app);
+
+        app.Run(requestDelegate);
+    }
+}
---
+++ Vostok.Metrics.AspNetCore/Helpers/EventHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Vostok.Metrics.AspNetCore.Helpers;
+
+internal static class EventHelper
+{
+    public static bool TryGetCounterValue(EventWrittenEventArgs eventData, string counterName, out long value)
+    {
+        value = 0;
+        if (eventData.Payload?.Count <= 0
+            || !(eventData.Payload?[0] is IDictionary<string, object> data)
+            || !data.TryGetValue("Name", out var n)
+            || !(n is string name)
+            || name != counterName) return false;
+
+        if (!data.TryGetValue("Mean", out var mean))
+            return false;
+        value = Convert.ToInt64(mean);
+        return true;
+    }
+
+    public static bool TryGetEventValue(EventWrittenEventArgs eventData, int payloadIndex, out object payload)
+    {
+        payload = default;
+        
+        if (eventData.Payload == null
+            || eventData.Payload.Count <= payloadIndex)
+            return false;
+
+        payload = eventData.Payload[payloadIndex];
+        return true;
+    }
+}

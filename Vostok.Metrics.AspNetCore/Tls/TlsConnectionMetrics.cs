560be239655cbb1dcfb73e95f18b31f648954398
Ilya Shumilin <shumilin@skbkontur.ru>
2022-08-11 14:46:23 +0500

    Add tls connections metrics

---
+++ Vostok.Metrics.AspNetCore/Tls/TlsConnectionMetrics.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Metrics.AspNetCore.Tls;
+
+/// <summary>
+/// <para>Set of counters associated with the Tls.</para>
+/// </summary>
+[PublicAPI]
+public class TlsConnectionMetrics
+{
+    /// <summary>
+    /// <para>The number of active TLS 1.0 sessions.</para>
+    /// <para>This metric is collected only for net5.0 and newer, zero for other platforms.</para>
+    /// </summary>
+    public long CurrentTls10Sessions { get; set; }
+
+    /// <summary>
+    /// <para>The number of active TLS 1.1 sessions.</para>
+    /// <para>This metric is collected only for net5.0 and newer, zero for other platforms.</para>
+    /// </summary>
+    public long CurrentTls11Sessions { get; set; }
+
+    /// <summary>
+    /// <para>The number of active TLS 1.2 sessions.</para>
+    /// <para>This metric is collected only for net5.0 and newer, zero for other platforms.</para>
+    /// </summary>
+    public long CurrentTls12Sessions { get; set; }
+
+    /// <summary>
+    /// <para>The number of active TLS 1.3 sessions.</para>
+    /// <para>This metric is collected only for net5.0 and newer, zero for other platforms.</para>
+    /// </summary>
+    public long CurrentTls13Sessions { get; set; }
+
+    /// <summary>
+    /// <para>The total number of failed TLS handshakes during the scrape period.</para>
+    /// <para>This metric is collected only for net5.0 and newer, zero for other platforms.</para>
+    /// <para>Note that this metric returns a diff (increment) between two consecutive observations: its value strongly depends on the observation period.</para>
+    /// </summary>
+    public long FailedTlsHandshakes { get; set; }
+}
